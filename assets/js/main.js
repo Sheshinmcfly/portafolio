efce2206db8b37f9c60651496a3c72bbb304e227
Sergio Palacios <s.palacios.hidalgo@gmail.com>
2021-12-01 16:51:57 -0300

    Subida de proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+/* Animación smooth scroll */
+$(document).ready(function () {
+
+    $('a').click(function () {
+
+        var gato = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(gato).offset().top - 70
+            },
+            800
+        )
+    })
+})
+
+/* Habilitar tooltip */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
